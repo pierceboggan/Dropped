e6e77a6e6983221772cd088225102ac63b9af5a2
Pierce Boggan <piboggan@microsoft.com>
2025-04-25 21:54:23 -0600

    Initial Commit

---
+++ Dropped/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dropped
+//
+//  Created by Pierce Boggan on 4/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dropped/DroppedApp.swift
@@ -0,0 +1,17 @@
+//
+//  DroppedApp.swift
+//  Dropped
+//
+//  Created by Pierce Boggan on 4/25/25.
+//
+
+import SwiftUI
+
+@main
+struct DroppedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
