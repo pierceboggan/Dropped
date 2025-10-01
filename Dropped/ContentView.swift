c7e8eadf6e4940c430b01bf7feec5783dbee2b92
Pierce Boggan <pierceboggan14@gmail.com>
2025-09-30 22:35:48 -0600

    Change label to My Workout Schedule on main

--- Dropped/ContentView.swift
+++ Dropped/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
             NavigationView {
                 List {
                     NavigationLink(destination: PlanSummaryView(hasCompletedOnboarding: $hasCompletedOnboarding)) {
-                        Label("Weekly Plan", systemImage: "calendar")
+                        Label("My Workout Schedule", systemImage: "list.bullet.clipboard")
                     }
                     NavigationLink(destination: {
                         // Inject dependencies for the generator
